f8fae60f8292b5cbe8e60c2f3265f9308c13a110
furkanigsiz <furkanigsizz@gmail.com>
2025-02-13 08:48:57 +0300

    SSL ve güvenlik ayarları eklendi

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://mathildamadaboutme.netlify.app' : '',
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NODE_ENV === 'production' ? 'https://mathildamadaboutme.netlify.app' : 'http://localhost:3000'
   }
 }
 
